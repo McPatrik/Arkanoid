55289bb32f50333a5ac4a41db8f3cafa3a2e9471
tatiana <tstartrekt@gmail.com>
2021-12-17 17:46:05 +0300

    .

--- js/game.js
+++ js/game.js
@@ -249,7 +249,7 @@ soundElement.addEventListener("click", audioManager);
 
 function audioManager() {
   let imgSrc = soundElement.getAttribute("src");
-  let soundImg = imgSrc == "img/SoundOn.png"? "img/SoundOff.png" : "img/SoundOn.png";
+  let soundImg = imgSrc == "/img/SoundOn.png"? "/img/SoundOff.png" : "/img/SoundOn.png";
 
   soundElement.setAttribute("src", soundImg);
 
